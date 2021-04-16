ff5509a27b21bfbeecde315ce67ccc057340739b
MeghaM-tinkuji <67791507+MeghaM-tinkuji@users.noreply.github.com>
2021-04-16 11:30:13 +0530

    Create test_project.h

---
+++ test_project.h
@@ -0,0 +1,35 @@
+/**
+ * @file project.h
+ * @author your name (you@domain.com)
+ * @brief 
+ * @version 0.1
+ * @date 2021-04-16
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+
+#ifndef __ALZHEIMERS_SOUL_SATHI_H__
+#define __ALZHEIMERS_SOUL_SATHI_H__
+
+#include <project.h>
+#include <unity.h>
+
+   /* Prototypes for all the test functions */
+   void opt(); 
+
+   void personnel_details();
+
+   void dob();
+
+   void family_details();
+
+   void medical_conditions();
+
+   void account_details();
+
+   void correct_pin();
+
+   void xit();
+
+#endif
